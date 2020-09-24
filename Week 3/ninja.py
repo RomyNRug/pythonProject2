5c118fa4248224dac3bc16649fdb048cf4b4380e
RomyN <romydejong99@gmail.com>
2020-09-24 18:40:39 +0200

    initial commit 2

---
+++ Week 3/ninja.py
@@ -0,0 +1,19 @@
+import turtle
+
+paper = turtle.Screen()
+leonardo = turtle.Turtle()
+
+colors = ['red','ýellow',]
+
+for element in [0,1,2,3,4,5]:
+    leonardo.forward(50)
+    leonardo.left(30)
+    leonardo.forward(50)
+    leonardo.left(40)
+
+
+
+print(element)
+
+paper.exitonclick()
+
