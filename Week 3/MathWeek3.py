5391d195064f474ee9377fca705b52fc094047e1
RomyN <romydejong99@gmail.com>
2020-10-01 14:03:58 +0200

    Initial Commit 2

--- Week 3/MathWeek3.py
+++ Week 3/MathWeek3.py
@@ -3,17 +3,71 @@ for i in range(1000):
     print("I like turtles")
 
 #2
-for i in range (12):
-    month = ["January", "Febuary","March", "April","May", "June","July", "August", "September", "October", "November", "December" ]
+month = ["January", "Febuary","March", "April","May", "June","July", "August", "September", "October", "November", "December" ]
+for i in month:
     print("The month is ",month)
 
 #3
-import turtle
-paper= turtle.Screen
-tess = turtle.Turtle()
-tess.left(3645)
+#import turtle
+#tess = turtle.Turtle()
+#tess.left(3645)
+#paper= turtle.Screen
+#paper.exitonclick()
 
-#4
+
+#4 a, b, c, d
+numbers= [12,10,32,3,66,17,42,99,20]
+for i in numbers:
+    print(i)
+
+numbers= [12,10,32,3,66,17,42,99,20]
+for i in numbers:
+    squared = (i ** 2)
+    print("the number is {} and its square is {}".format(i,squared))
+
+total=0
+numbers= [12,10,32,3,66,17,42,99,20]
+for i in numbers:
+    total +=i
+print(total)
+
+product=1
+numbers= [12,10,32,3,66,17,42,99,20]
+for i in numbers:
+    product *=i
+    print(product)
+
+#5 Equilateral Triangle
+#import turtle
+#paper=turtle.Screen()
+#john=turtle.Turtle()
+#for i in range(3):
+  #  john.forward(100)
+   # john.left(120)
+#paper.exitonclick()
+
+#Hexagon                    ocatgon is (forward 75 and left(360/8)
+#import turtle
+#paper=turtle.Screen()
+#carlos=turtle.Turtle()
+#for i in range(6):
+ #   carlos.forward(50)
+  #  carlos.left(60)
+#paper.exitonclick()
+
+#square
+#import turtle
+#paper=turtle.Screen()
+#manual= turtle.Turtle()
+#for i in range (4):
+ #   manual.forward(100)
+ #   manual.left(360/4)
+#paper.exitonclick
+
+
+
+
+
 
 
 
