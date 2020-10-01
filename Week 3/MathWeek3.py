5c318b67ab994506279c32734cf0bdfdc9243cbd
RomyN <romydejong99@gmail.com>
2020-10-01 11:47:32 +0200

    Initial Commit 2

--- Week 3/MathWeek3.py
+++ Week 3/MathWeek3.py
@@ -7,3 +7,17 @@ for i in range (12):
     month = ["January", "Febuary","March", "April","May", "June","July", "August", "September", "October", "November", "December" ]
     print("The month is ",month)
 
+#3
+import turtle
+paper= turtle.Screen
+tess = turtle.Turtle()
+tess.left(3645)
+
+#4
+
+
+
+    
+
+
+
