f2bd40b51b776483bcbe795f0b0b74bfff235e01
RomyN <romydejong99@gmail.com>
2020-10-14 17:28:10 +0200

    Initial Commit 2

---
+++ Week 5/Exercise5.1.19.py
@@ -0,0 +1,19 @@
+#1 Python Console
+
+#2
+
+prefixes = ("JKLMNOPQ")
+suffix = "ack"
+for letter in prefixes:
+    if letter == "O" or letter == "Q":
+        print (letter + "u" + suffix)
+    print (letter + suffix)
+
+#3
+word = "banana"count = 0
+count = 0
+for letter in word:
+    if letter == "a":
+        count += 1
+    print(count)
+
