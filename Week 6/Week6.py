19bcd4e86f657eb730829496105d1b72db2083e9
RomyN <romydejong99@gmail.com>
2020-10-19 15:39:29 +0200

    Initial Commit 2

--- Week 6/Week6.py
+++ Week 6/Week6.py
@@ -32,3 +32,31 @@ new_pix = np.array(new_pix).astype('uint8')
 
 im2 = Image.fromarray(new_pix)
 im2.show()
+
+
+def fade_bw(image):
+    pixels = np.array(image, dtype='uint8')
+    new_pix = [[(sum(pixels[row, col]) / 7) for col in range(pixels.shape[1])] for row in range(pixels.shape[0])]
+    new_image = Image.fromarray(np.array(new_pix, dtype='uint8'))
+    new_image.show()
+
+    return new_image
+
+
+def fade_color(image):
+    pixels = np.array(image, dtype='int8')
+
+    for row in range(pixels.shape[0]):
+        for col in range(pixels.shape[1]):
+            pixels[row, col, 0] *= 0.5
+            pixels[row, col, 1] *= 0.5
+            pixels[row, col, 2] *= 0.5
+
+    new_image = Image.fromarray(pixels)
+    new_image.show()
+
+    return new_image
+
+im = Image.open("fruit.jpg")
+im2 = fade_bw(im)
+im2.save("fruit_fade.png", "PNG")
