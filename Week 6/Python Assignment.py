c251d34db4bbcc92c0e9d95c9f18929fad45789f
RomyN <romydejong99@gmail.com>
2020-10-21 16:02:08 +0200

    Assignment

--- Week 6/Python Assignment.py
+++ Week 6/Python Assignment.py
@@ -1,52 +1,43 @@
 #Revisiting exercise 2.4.2
 
-distance = input("How far is your destination in kilometers?")
-transportation= input("What was your mode of transportation?")
+distance = input("How far is your destination in kilometers? Please enter a number here:")
+transportation= input("What was your mode of transportation? Choose between car, bike or walk: ")
 distance_time= float(distance)
-total_km= 0
-total_time= 0 # in minutes
-avg_bike= 11 #11 kilometers/hour
-total_break_time= 0
-tot_bike_time=0
-total_bike_time= 0
-extra_bike_time= 0
+total_time= 0
 
 for _ in distance:
     if transportation == "car":
-        avg_car_km_h= 50    #average km per hour
-        total_km += avg_car_km_h
+        avg_car_km_h= 50
+        total_time= distance_time/avg_car_km_h
         gas_stop= input("Did you have to stop for gas during your trip?")
         if gas_stop == "yes":
-            total_time += 10 #minutes
-        ####can add other variables
-    if transportation == "bike":
-        avg_bike_km_h =11
-        total_km += avg_bike_km_h
-        tot_bike_time= total_km/avg_bike_km_h
+            total_time += (10/60)
+
+    elif transportation == "bike":
+        avg_bike_km_h= 11
+        total_time= distance_time/avg_bike_km_h
         bike_shed= input("Did you have to take the bike out of the shed?")
         if bike_shed == "yes":
-            extra_bike_time = (5/60)
-    total_bike_time = extra_bike_time + tot_bike_time
-    if transportation == "walk":
-        externality_walk = input("Did you experience any delay due to fatigue and therefore took breaks?") #why does it come back here?
+            total_time += (5/60)
+
+    elif transportation == "walk":
         avg_walk_km_h= 5
-        total_km += avg_walk_km_h
-        walk_time= (distance_time/avg_walk_km_h)
+        total_time= (distance_time/avg_walk_km_h)
+        externality_walk = input("Did you experience any delay due to fatigue and therefore took breaks? Type yes or no: ")
         if externality_walk == "yes":
-            amount_breaks= input("how many breaks did you take?")
+            amount_breaks= input("How many breaks did you take? Please enter the number here:")
             if amount_breaks == "yes":
-                avg_break_time = (0.25)#15/60
-                amount_breaks = float()
-                total_break_time= amount_breaks*avg_break_time
-                walk_time += total_break_time
-        #elif externality_walk == "no":
-         #       total_break_time = 0
+                breaks_ = float(amount_breaks)
+                total_time += (breaks_*(15/60))
 
-total_time= walk_time+ total_break_time + total_bike_time #why is it not adding break time?
-print("The time you took to your destination is ",total_time, " ,defined in hours including externalities")
+# if a new mode of transportation is added such as train. a new if transportation =="train"
+# should be added with its corresponding average km/h and externalities in the case of those.
 
+    if transportation == "train":
+        avg_train_km_h= 150
+        total_time= (distance_time/avg_train_km_h)
 
-
+print("The time you took to your destination is ",round(total_time,1), "hours ,defined in hours including externalities")
 
 
 
